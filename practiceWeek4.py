4cc80f1c0233b6427015acd4896c7aa419970650
Kaushal Kishor <radhe@Kaushals-MacBook-Air.local>
2026-05-23 16:00:42 +0530

    week4 exercise

---
+++ practiceWeek4.py
@@ -0,0 +1,20 @@
+import requests
+from dotenv import load_dotenv
+import os
+response=requests.get("https://jsonplaceholder.typicode.com/users")
+print(response.status_code)
+data=response.json()
+#print(type(data))
+#print(data)
+for d in data:
+    print(d["name"]," --- ",d["email"])
+
+response2=requests.get("https://jsonplaceholder.typicode.com/users/1")
+data1=response2.json()
+print(data1)
+print(data1["name"], data1["email"],data1["address"]["city"])
+#print(data==data1)
+
+load_dotenv()
+key=os.getenv("MY_API_KEY")
+print(key)
